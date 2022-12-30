0f7f82d422b03e3525a8c9e8b4bfc2a92a7735a4
Agustin Alba Chicar <ag.albachicar@gmail.com>
2022-12-30 17:02:40 +0100

    Solve nits.
    Signed-off-by: Agustin Alba Chicar <ag.albachicar@gmail.com>

--- maliput_ros/test/maliput_ros/utils/yaml_parser_test.cc
+++ maliput_ros/test/maliput_ros/utils/yaml_parser_test.cc
@@ -1,6 +1,6 @@
 // BSD 3-Clause License
 //
-// All rights reserved.
+// Copyright (c) 2022, Woven Planet. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
